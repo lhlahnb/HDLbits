ee27f9972c552e6b934cb5cd8ba0f7168dfa228e
lhlahnb <132525221+lhlahnb@users.noreply.github.com>
2024-01-18 15:10:31 +0800

    前七个

---
+++ one_step.v
@@ -0,0 +1,5 @@
+module top_modle (
+    output one
+);
+    assign one = 1'b1;
+endmodule
---
+++ zero.v
@@ -0,0 +1,13 @@
+module top_modle (
+    output zero
+);
+
+endmodule
+
+
+module top_modle (
+    zero
+);
+  output zero;
+endmodule
+//两种方式都可以构建一个没有输入，只有输出，并且输出为0的电路
